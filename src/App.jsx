bb855e1aba711fc6c9c160026cbb31b41ce46d63
McMaldo <maldonado.ignacio.pablo@gmail.com>
2026-03-04 18:35:10 -0300

    FEAT #006 - new material + new material + sections for years and schools

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import LinksGroup from "./components/LinksGroup";
-import courses from "./links.json";
+import sections from "./links.json";
+import Heading from "./components/Heading.jsx";
 
 export default function App() {
   return (
-    <>
-      {courses.map((course, index) => (
-        <LinksGroup key={index} course={course} />
+    <main className="w-full max-w-200 min-h-dvh text-white flex flex-col items-center justify-center gap-4 py-8 px-4">
+      <div className="relative w-full h-60 rounded-md overflow-hidden select-none">
+        <Heading />
+        <div className="absolute top-0 left-0 size-full flex flex-col gap-4 items-center justify-center px-4">
+          <h1 className="text-3xl text-center">Clases con MaldoProfe</h1>
+          <div className="flex gap-4 flex-wrap justify-center">
+            {["Computación", "Informática", "NTICX"].map((tag, tagIndex) => (
+              <span
+                key={tagIndex}
+                className="border border-gray-800 bg-gray-900 py-1 px-3 rounded-full text-sm"
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+      {sections.map((section, index) => (
+        <div key={index} className="w-full flex flex-col gap-4">
+          <span className="pt-1 text-gray-400 font-light w-[calc(100%-1.5rem)] border-b border-gray-900 text-md">
+            {section.name}
+          </span>
+          {section.courses.map((course, index) => (
+            <LinksGroup key={index} course={course} />
+          ))}
+        </div>
       ))}
-    </>
+    </main>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Heading from "./components/Heading.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <main className="w-full max-w-200 min-h-dvh text-white flex flex-col items-center justify-center gap-4 py-8 px-4">
-      <div className="relative w-full h-60 rounded-md overflow-hidden select-none">
-        <Heading />
-        <div className="absolute top-0 left-0 size-full flex flex-col gap-4 items-center justify-center px-4">
-          <h1 className="text-3xl text-center">Clases con MaldoProfe</h1>
-          <div className="flex gap-4 flex-wrap justify-center">
-            {["Computación", "Informática", "NTICX"].map((tag, tagIndex) => (
-              <span
-                key={tagIndex}
-                className="border border-gray-800 bg-gray-900 py-1 px-3 rounded-full text-sm"
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-        </div>
-      </div>
-      <App />
-    </main>
+    <App />
   </StrictMode>,
 );
